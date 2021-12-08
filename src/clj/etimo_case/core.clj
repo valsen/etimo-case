5ab28dbd601dfb2f762a11f01c8d0a485ad0cf71
valsen <valsen@users.noreply.github.com>
2021-12-08 14:17:39 +0100

    Refactor inventory to global state, refactor handlers, remove user-provided initial inventory

--- src/clj/etimo_case/core.clj
+++ src/clj/etimo_case/core.clj
@@ -1,21 +1,18 @@
 (ns etimo-case.core
-  "This is a simple but extendable implementation of a console application
+  "This is a simple but extensible implementation of a console application
    with the following functionality currently implemented:
    1) The user can sell products with the command 'S' followed by a positive integer, e.g. 'S5'.
    2) The user can re-stock products with the command 'I' followed by a positive integer, e.g. 'I3'.
    3) The user can query the current inventory count with the command 'L'.
-   4) The user can optionally set an initial inventory count by supplying it as a command-line arg to the program.
 
-   My goal with this implementation is to eliminate global state and the complexity that usually comes with managing it.
-   I have tried to keep each function as 'pure' as possible, i.e. with few or no side-effects.
-
-   I have also tried to make it easy to add new user commands and behavior, by defining a command
-   as a map that contains the command's handler function, the current inventory and
-   any other data needed by the handler function.
+   User commands are routed using the parse-command function, which is a pure function that
+   returns a map with :handler and :args keys for the given command.
+   New commands can be easily added to parse-command, and creating a new handler
+   is as simple as defining a function that takes a map as its only argument.
 
    I've included test cases right in the function definitions,
    between the docstring and function parameters. This way it's easy to
-   quickly get an understanding of what a function accomplishes, by looking
+   quickly get an understanding of what a function does, by looking
    at the inputs and outputs of the test cases right along the source code.
 
    With clojure command line tools installed, you can run the program with 'clj -M:run',
@@ -30,125 +27,148 @@
 ;; optionally followed by one or more digits as a numerical argument.
 (def command-pattern #"^([a-zA-Z]+)([\d]+)?$")
 
+;; The inventory count is the only global, mutable state of the program.
+;; For this specific implementation (single-user console app),
+;; it would arguably be better to remove it from global state and just pass
+;; a value around between the functions to minimize/eliminate side-effects.
+;; But keeping it in global state makes sense for future multi-client support,
+;; e.g. if the program is made into a web app where the inventory state would be
+;; shared between all clients.
+(def inventory (atom 0))
+
+(defn set-inventory!
+  "Resets the state of the inventory atom to the value of new-inventory."
+  [new-inventory]
+  (reset! inventory new-inventory))
+
 (defn get-user-input
   "Asks the user to enter a command. Stores the user's input with an :input key
    in the supplied data map, which is then returned."
-  [data]
+  []
   (print "Enter a command: ")
   (flush)
-  (assoc data :input (read-line)))
+  (read-line))
 
-(defn update-inventory
-  "Returns a map with the updated :inventory as the sum of the current inventory and the adjustment amount. 
-   Throws exception on invalid adjustment."
+(defn update-inventory!
+  "This function has the side-effect of applying function f to the
+   current value of the inventory atom and x. Throws exception on invalid updates."
   {:test (fn []
            ;; negative adjustment decreases inventory
-           (is (= (update-inventory {:inventory 10
-                                     :adjustment -3})
-                  {:inventory 7}))
+           (is (= (do
+                    (set-inventory! 10)
+                    (update-inventory! - 3))
+                  7))
            ;; postitive adjustment increases inventory
-           (is (= (update-inventory {:inventory 0
-                                     :adjustment 1})
-                  {:inventory 1}))
+           (is (= (do
+                    (set-inventory! 0)
+                    (update-inventory! + 1))
+                  1))
            ;; adjustment cannot be nil
-           (is (thrown? Exception (update-inventory {:inventory 0
-                                                     :adjustment nil})))
+           (is (thrown? Exception (update-inventory! + nil)))
            ;; adjustment cannot be 0
-           (is (thrown? Exception (update-inventory {:inventory 1
-                                                     :adjustment 0})))
-           ;; inventory cannot become negative. TODO: should the available stock be sold?
-           (is (thrown? Exception (update-inventory {:inventory 1
-                                                     :adjustment -2}))))}
-  [{:keys [inventory adjustment]}]
-  (when (nil? adjustment)
-    (throw (ex-info "Adjustment cannot be nil." {})))
-  (when (zero? adjustment)
-    (throw (ex-info "Adjustment cannot be 0." {})))
-  (when (< (+ inventory adjustment) 0)
+           (is (thrown? Exception (do
+                                    (set-inventory! 1)
+                                    (update-inventory! - 0))))
+           ;; inventory cannot become negative.
+           (is (thrown? Exception (do
+                                    (set-inventory! 1)
+                                    (update-inventory! - 2)))))}
+  [f x]
+  (when (nil? x)
+    (throw (ex-info "The command's numerical argument cannot be nil." {})))
+  (when (identical? (f @inventory x) @inventory)
+    (throw (ex-info "The command has no effect on the inventory." {})))
+  (when (< (f @inventory x) 0)
     (throw (ex-info "Inventory cannot be negative." {})))
-  {:inventory (+ inventory adjustment)})
+  (swap! inventory f x))
 
-(defn print-inventory
+(defn sell!
+  "Removes qty from inventory."
+  {:test (fn []
+           ;; tests a sequence of three calls to sell!
+           (set-inventory! 3)
+           (is (= (sell! {:qty 2}) 1))
+           (is (= (sell! {:qty 1}) 0))
+           (is (thrown? Exception (sell! {:qty 1}))))}
+  [{:keys [qty]}]
+  (update-inventory! - qty))
+
+(defn re-stock!
+  "Adds qty to inventory."
+  {:test (fn []
+           ;; tests a sequence of two calls to re-stock!
+           (set-inventory! 0)
+           (is (= (re-stock! {:qty 2}) 2))
+           (is (= (re-stock! {:qty 9}) 11)))}
+  [{:keys [qty]}]
+  (update-inventory! + qty))
+
+(defn get-inventory-message
+  "Returns a message containing the current inventory count.
+   For a web app, this could be used as the handler function
+   for the command 'L'."
+  []
+  (str "The current inventory is: " @inventory))
+
+(defn print-inventory-message
   "Prints the inventory as a side-effect and returns the function's argument unmodified."
-  [data]
-  (println (str "The current inventory is: " (:inventory data)))
-  data)
+  [_]
+  (println (get-inventory-message)))
 
 (defn parse-command
-  "Parses the raw user-input into a map representing a program command. 
+  "This function acts as a router for incoming commands. 
    The output map contains a :handler function and the current :inventory,
    as well as any additional keys needed by the handler function.
-   Throws exception on invalid user input.
+   An exception is thrown if command is invalid/unrecognized.
 
    Adding new commands and program behavior can be easily done here by
    extending the condp form with more command names and handler functions,
    as long as the command conforms to the command-pattern regex.
-   As an example, I have added an 'exit' command to terminate the program.
-"
+   As an example, I have added an 'exit' command to terminate the program."
   {:test (fn []
-           (is (= (parse-command {:input "S12"
-                                  :inventory 0})
-                  {:handler update-inventory
-                   :inventory 0
-                   :adjustment -12}))
-           (is (= (parse-command {:input "I3"
-                                  :inventory 0})
-                  {:handler update-inventory
-                   :inventory 0
-                   :adjustment 3}))
-           (is (= (parse-command {:input "L"
-                                  :inventory 5})
-                  {:handler print-inventory
-                   :inventory 5}))
+           (is (= (parse-command "S12")
+                  {:handler sell!
+                   :args {:qty 12}}))
+           (is (= (parse-command "I3")
+                  {:handler re-stock!
+                   :args {:qty 3}}))
+           (is (= (parse-command "L")
+                  {:handler print-inventory-message}))
            ;; 'X' is not a valid command
-           (is (thrown? Exception (parse-command {:input "X1"
-                                                  :inventory 0})))
+           (is (thrown? Exception (parse-command "X1")))
            ;; The 'S' command must be uppercase
-           (is (thrown? Exception (parse-command {:input "s1"
-                                                  :inventory 0})))
+           (is (thrown? Exception (parse-command "s1")))
            ;; commands cannot have trailing characters after its numerical component.
-           (is (thrown? Exception (parse-command {:input "I5xxx"
-                                                  :inventory 0}))))}
-  [{:keys [input inventory]}]
+           (is (thrown? Exception (parse-command "I5xxx"))))}
+  [input]
   (let [[_ action qty] (re-find command-pattern input)]
     (condp = action
-      "I" {:handler update-inventory
-           :inventory inventory
-           :adjustment (read-string qty)}
-      "S" {:handler update-inventory
-           :inventory inventory
-           :adjustment (* -1 (read-string qty))}
-      "L" {:handler print-inventory
-           :inventory inventory}
+      "I" {:handler re-stock!
+           :args {:qty (read-string qty)}}
+      "S" {:handler sell!
+           :args {:qty (read-string qty)}}
+      "L" (if qty
+            (throw (ex-info "Command 'L' does not take any argument." {}))
+            {:handler print-inventory-message})
       "exit" {:handler (fn [_] (System/exit 0))}
-      (throw (ex-info "Command not recognized, please try again" {})))))
+      ;; throw exception if no match
+      (throw (ex-info "Command not recognized, please try again." {})))))
 
-(defn main-loop
-  "This function is like a data pipeline, where each step's output is a 
-   map which serves as the next step's input, starting with the initial inventory.
-   At the end of the pipeline, the function recurses with the modified data map,
-   which at that point contains the updated inventory. If any step throws an exception,
-   its message is printed, after which main-loop recurses with the same (i.e. unmodified)
-   data as it was called with."
-  [{:keys [inventory] :as data}]
+(defn console-loop
+  "This function parses a command from user input, calls its handler function and then calls itself.
+   If an exception is caught, an error message is printed before recursing."
+  []
   (try
-    (as-> data $
-      (get-user-input $)
+    (as-> (get-user-input) $
       (parse-command $)
-      ((:handler $) $)
-      (main-loop $))
+      ((:handler $) (:args $)))
+    (console-loop)
     (catch Exception e
       (println (.getMessage e))
-      (main-loop {:inventory inventory}))))
+      (console-loop))))
 
 (defn -main
-  "The program's entrypoint. The user may provide a non-negative integer
-   argument for the initial inventory. If no initial inventory is provided,
-   it defaults to 0."
-  ([]
-   (main-loop {:inventory 0}))
-  ([inventory]
-   (if-not (re-find #"^\d+$" inventory)
-     (println "Invalid initial inventory provided. Exiting...")
-     (main-loop {:inventory (read-string inventory)}))))
+  "The program's entrypoint."
+  []
+  (console-loop))
 
